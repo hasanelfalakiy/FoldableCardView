34bfd7eae00743a382644e4d1b555fcd968e3005
andihasan97 <andihasanelfalakiy@gmail.com>
2025-07-03 10:31:55 +0700

    release v1.2

--- foldablecardview/build.gradle.kts
+++ foldablecardview/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
 
                 groupId = "com.github.hasanelfalakiy"
                 artifactId = "foldable-cardview"
-                version = "1.0"
+                version = "1.2"
             }
         }
     }
